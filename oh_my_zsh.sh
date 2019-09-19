8d7f34b5cd0de9f407c48d59d328a39b8b76a878
Stephane <eicher31@hotmail.com>
2019-09-19 09:56:59 +0200

    Update script oh_my_zsh

--- oh_my_zsh.sh
+++ oh_my_zsh.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 sudo apt update
-sudo apt install zsh
+sudo apt -y install zsh
 sudo usermod -s /usr/bin/zsh $(whoami)
+chsh -s $(which zsh)
+sudo apt -y install powerline fonts-powerline zsh-syntax-highlighting
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+git clone https://github.com/romkatv/powerlevel10k.git /usr/share/powerlevel10k
+echo 'source /usr/share/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
