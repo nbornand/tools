183f00b73583e0af9787c68784931310c4a2e88c
Stephane <eicher31@hotmail.com>
2019-09-26 08:39:21 +0200

    Fix right permission in oh_my_zsh.sh

--- oh_my_zsh.sh
+++ oh_my_zsh.sh
@@ -5,6 +5,6 @@ sudo usermod -s /usr/bin/zsh $(whoami)
 chsh -s $(which zsh)
 sudo apt -y install powerline fonts-powerline zsh-syntax-highlighting
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-git clone https://github.com/romkatv/powerlevel10k.git /usr/share/powerlevel10k
+sudo git clone https://github.com/romkatv/powerlevel10k.git /usr/share/powerlevel10k
 echo 'source /usr/share/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
